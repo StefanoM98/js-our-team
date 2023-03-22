2814b04540fe07a507a9b935505a3b8c95306f30
StefanoM98 <smartino030@gmail.com>
2023-03-22 14:28:44 +0100

    Add: Cards with photo

--- js/script.js
+++ js/script.js
@@ -5,43 +5,55 @@ const team = [
         nome: "Wayne",
         cognome: "Barnett",
         lavoro: "Founder & CEO",
-        fotoProfilo: "wayne-barnett-founder-ceo.jpg"
+        foto: "wayne-barnett-founder-ceo.jpg"
     },
     {
         nome: "Angela",
         cognome: "Caroll",
         lavoro: "Chief Editor",
-        fotoProfilo: "angela-caroll-chief-editor.jpg"
+        foto: "angela-caroll-chief-editor.jpg"
     },
     {
         nome: "Walter",
         cognome: "Gordon",
         lavoro: "Office Manager",
-        fotoProfilo: "walter-gordon-office-manager.jpg"
+        foto: "walter-gordon-office-manager.jpg"
     },
     {
         nome: "Angela",
         cognome: "Lopez",
         lavoro: "Social Media Manager",
-        fotoProfilo: "angela-lopez-social-media-manager.jpg"
+        foto: "angela-lopez-social-media-manager.jpg"
     },
     {
         nome: "Scott",
         cognome: "Estrada",
         lavoro: "Developer",
-        fotoProfilo: "scott-estrada-developer.jpg"
+        foto: "scott-estrada-developer.jpg"
     },
     {
         nome: "Barbara",
         cognome: "Ramos",
         lavoro: "Graphic Designer",
-        fotoProfilo: "barbara-ramos-graphic-designer.jpg"
+        foto: "barbara-ramos-graphic-designer.jpg"
     }
 ]
 
-console.log(team);
+const cardRow = document.querySelector(".row");
 
+let cards = ""
 for (let i = 0; i < team.length; i++) {
-    const curTeam = team[i];
-    console.log(curTeam);
+    const myTeam = team[i];
+    cards += `
+    <div class="col p-4">
+        <div class="card">
+            <img src="img/${myTeam.foto}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-text text-center">${myTeam.nome} ${myTeam.cognome}</h5>
+                <p class="card-text text-center">${myTeam.lavoro}</p>
+            </div>
+        </div>
+    </div>
+    `;
 }
+cardRow.innerHTML = cards
