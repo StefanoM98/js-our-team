e65609c29d3cae92295b7836100beda2fa242552
StefanoM98 <smartino030@gmail.com>
2023-03-22 12:58:20 +0100

    Add: array complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-alert("ciao")
+// CREIAMO L'ARREY DEL TEAM
+
+const team = [
+    {
+        nome: "Wayne",
+        cognome: "Barnett",
+        lavoro: "Founder & CEO",
+        fotoProfilo: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: "Angela",
+        cognome: "Caroll",
+        lavoro: "Chief Editor",
+        fotoProfilo: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Walter",
+        cognome: "Gordon",
+        lavoro: "Office Manager",
+        fotoProfilo: "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Angela",
+        cognome: "Lopez",
+        lavoro: "Social Media Manager",
+        fotoProfilo: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: "Scott",
+        cognome: "Estrada",
+        lavoro: "Developer",
+        fotoProfilo: "scott-estrada-developer.jpg"
+    },
+    {
+        nome: "Barbara",
+        cognome: "Ramos",
+        lavoro: "Graphic Designer",
+        fotoProfilo: "barbara-ramos-graphic-designer.jpg"
+    }
+]
